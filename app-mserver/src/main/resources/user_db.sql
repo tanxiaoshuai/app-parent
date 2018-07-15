5376d81482230fe228745e03ba6f84b834d95b5c
tanshuai <tanshuai@sobey.com>
2018-07-15 22:45:48 +0800

    用户数据库

---
+++ app-mserver/src/main/resources/user_db.sql
@@ -0,0 +1,41 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.7.18 : Database - user_db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`user_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `user_db`;
+
+/*Table structure for table `t_user` */
+
+DROP TABLE IF EXISTS `t_user`;
+
+CREATE TABLE `t_user` (
+  `id` varchar(32) NOT NULL,
+  `user_name` varchar(25) DEFAULT NULL,
+  `pass_word` varchar(32) DEFAULT NULL,
+  `tel` varchar(15) DEFAULT NULL,
+  `register_time` varchar(25) DEFAULT NULL,
+  `head_url` text,
+  `remark` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_user` */
+
+insert  into `t_user`(`id`,`user_name`,`pass_word`,`tel`,`register_time`,`head_url`,`remark`) values ('881a5581e7f14435ba771621981696e','谭帅','E10ADC3949BA59ABBE56E057F20F883E','13088094976','2018-07-14 22:10:43',NULL,'备注');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
